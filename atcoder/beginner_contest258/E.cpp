2768225c7c5ca373189ec179ab00675571b128ab
YingFan-Chen <b07502024@ntu.edu.tw>
2022-07-04 16:21:44 +0800

    new-v

--- atcoder/beginner_contest258/E.cpp
+++ atcoder/beginner_contest258/E.cpp
@@ -2,10 +2,52 @@
 #define ll long long
 #define ll_max LONG_LONG_MAX
 #ifndef ONLINE_JUDGE
-#include "C:\Users\josep\code\c++\lib\debug"
+#include "..\..\lib\debug"
 #endif
 using namespace std;
 
-int main(){
+ll f(vector<ll> &sum, ll i, ll x, ll n){
+    ll l = i, r = i + n, res = 0;
+    res = n * (x / sum[n - 1]);
+    x %= sum[n - 1];
+    if(x == 0) return res;
+    while(l + 1 < r){
+        ll mid = (l + r) >> 1;
+        ll section;
+        if(i == -1) section = sum[mid];
+        else section = sum[mid] - sum[i];
+        if(section >= x) r = mid;
+        else l = mid;
+    }
+    res += r - i;
+    return res; 
+}
 
+int main(){
+    ll n, q, x;
+    cin >> n >> q >> x;
+    vector<ll> W(n, 0);
+    for(int i = 0; i < n; i ++) cin >> W[i];
+    vector<ll> sum(n << 1, 0);
+    sum[0] = W[0];
+    for(int i = 1; i < n << 1; i ++) sum[i] = sum[i - 1] + W[i % n];
+
+    vector<ll> boxes;
+    unordered_map<ll, ll> um;
+    ll i = 0;
+    while(um.find(i) == um.end()){
+        um[i] = boxes.size();
+        ll tmp = f(sum, i - 1, x, n);
+        boxes.push_back(tmp);
+        i += tmp;
+        i %= n;
+    }
+    ll start = um[i], mod = boxes.size() - start;
+    for(int j = 0; j < q; j ++){
+        ll tmp;
+        cin >> tmp;
+        tmp --;
+        if(tmp >= start) tmp = start + (tmp - start) % mod;
+        cout << boxes[tmp] << endl;
+    }
 }
--- socket/new_server.cpp
+++ socket/new_server.cpp
@@ -3,11 +3,23 @@
 #include <sqlite3.h>
 using namespace std;
 #define server_port 8080
+#define max_client 1000
 
 sqlite3* setup_environment();
+int setup_master_socket(struct sockaddr*);
 
 int main(){
-    sqlite3 *db = setup_environment();
+    struct sockaddr_in address;
+    sqlite3 *db;
+    int master_sock;
+
+    db = setup_environment();
+
+    address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(server_port);
+
+    master_sock = setup_master_socket((struct sockaddr *) &address);
 
     
 }
@@ -26,14 +38,14 @@ sqlite3* setup_environment(){
         fprintf(stderr, "Open database successfully....\n");
     }
 
-    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS accounts(account varchar(16), password varchar(16));", NULL, NULL, &err);
+    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS accounts(account varchar(128), password varchar(128));", NULL, NULL, &err);
     if(rc != SQLITE_OK){
         fprintf(stderr, "Fail to create account table : %s\n", err);
     }else{
         fprintf(stderr, "Create account table successfully....\n");
     }
 
-    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS relations(client varchar(16), friends varchar(16), chatroom varchar(50));", NULL, NULL, &err);
+    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS relations(client varchar(128), friends varchar(128), chatroom varchar(1024));", NULL, NULL, &err);
     if(rc != SQLITE_OK){
         fprintf(stderr, "Fail to create relation table : %s\n", err);
     }else{
@@ -43,6 +55,32 @@ sqlite3* setup_environment(){
     return db;
 }
 
-void create_socket(){
-    
+int setup_master_socket(struct sockaddr *address){
+    int master_sock;
+    int opt = 1;
+    struct sockaddr_in address;
+
+    if(master_sock = socket(AF_INET , SOCK_STREAM , 0)) == 0){
+        perror("Fail to socket");
+    }else{
+        fprintf(stderr, "Socket successfully....\n");
+    }
+
+    if(setsockopt(master_sock, SOL_SOCKET, SO_REUSEADDR | SOREUSEPORT, &opt, sizeof(opt))){
+        perror("Fail to setsockopt");
+    }else{
+        fprintf(stderr, "Setsockopt successfully....\n");
+    }
+
+    if(bind(master_sock, address, sizeof(*address)) < 0){  
+        perror("Fail to bind");
+    }else{
+        fprintf(stderr, "Bind successfully....\n");
+    }
+
+    if(listen(master_sock, max_client) < 0){
+        perror("Fail to listen");
+    }else{
+        fprintf(stderr, "Listen successfully....\n");
+    }
 }
