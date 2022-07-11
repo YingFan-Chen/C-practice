39311e416d02107e3b266dad2eee86e56c960cfb
YingFan-Chen <b07502024@ntu.edu.tw>
2022-07-11 15:00:23 +0800

    cm

--- NTT/new.cpp
+++ NTT/new.cpp
@@ -1,29 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int16_t _nm = 512, _mod = 17, _m = 16, _n = 32;   
-// x^n = -1, y^n = -1 
+const int16_t _mn = 512, _mod = 17, _m = 16, _n = 32;
 
-void schonhage(int16_t* p1, int16_t* p2, int16_t* res, int16_t nm, int16_t m, int16_t n){
-    int16_t _p1[nm << 1], _p2[nm << 1];
-    int16_t i = 0, j = 0, k;
+const int16_t C512[60] = {  8, -8, 24, -24,
+                            4, -4, 20, -20, 12, -12, 28, -28,
+                            2, -2, 18, -18, 10, -10, 6, -6, 22, -22, 14, -14, 30, -30,
+                            1, -1, 17, -17, 9, -9, 25, -25, 5, -5, 21, -21, 3, -3, 19, -19, 11, -11, 27, -27, 7, -7, 23, -23, 15, -15, 31, -31  }; 
+                
 
-    for(; i < nm >> 1; ++ i){
-        for(k = 0; k < m; ++ k){
-            _p1[j] = p1[i];
-            _p1[j + m] = - p1[i];
-            ++ j;
-        }
-        j += m;
-    }
-    for(; i < nm; ++ i){
-        for(k = 0; k < m; ++ k){
-            _p1[j + m] = _p1[j] = p1[i];
-            ++ j;
-        }
-        j += m;
-    }
+void schonhage(int16_t* p1, int16_t* p2, int16_t* res, int16_t mn, int16_t m, int16_t n){
+    int16_t _p1[mn << 1], _p2[mn << 1];
     
+    int16_t i = 0, j = 0;
+    while(i < mn >> 1){
+        for(int16_t _ = 0; _ < m; ++ _){
+            _p1[j] = _p1[j + mn] = p1[i];
+            _p2[j] = _p2[j + mn] = p2[i];
+            ++ i; ++ j;
+        }
+        j += m;
+    }
+    j = m;
+    while(i < mn){
+        for(int16_t _ = 0; _ < m; ++ _){
+            _p1[j] = p1[i];
+            _p1[j + mn] = - p1[i];
+            _p2[j] = p2[i];
+            _p2[j + mn] = - p2[i];
+            ++ i; ++ j; 
+        }
+        j += m;
+    }
+
+    int16_t C[n << 2];
+    C[0] = m >> 1;
+    C[1] = - C[0];
+    C[2] = C[0] + m;
+    C[3] = - C[2];
+    int16_t base = 0, top = 4;
+
+    for(int16_t k = m; k > 1; k >>= 1){
+        
+    }  
 }
 
 
---
+++ NTT/schonhage.c
@@ -0,0 +1,61 @@
+#include <stdint.h>
+
+void makeC(int16_t m, int16_t* C){
+    C[0] = m, C[1] = -m;
+    int16_t base = 0, top = 2;
+    while(C[base] ^ 1){
+        int16_t tmp = C[base];
+        if(tmp >= 0){
+            tmp >>= 1;
+            C[top] = tmp;
+            ++ top;
+            C[top] = -tmp;
+            ++ top; 
+        }else{
+            tmp = -tmp;
+            tmp >>= 1;
+            tmp += m;
+            C[top] = tmp;
+            ++ top;
+            C[top] = -tmp;
+            ++ top;
+        }
+    }
+}
+
+void Cooley_Tukey(int16_t l, int16_t r, int16_t n, int16_t *p){
+    
+}
+
+
+void schonhage(int16_t m, int16_t n, int16_t* p1, int16_t* p2, int16_t* p3, int16_t* C){
+    int16_t mn = m * n;
+    int16_t p1_[mn << 1], p2_[mn << 1];
+
+    int16_t i = 0, j = 0;
+    while(i < mn >> 1){
+        for(int16_t _ = 0; _ < m; ++ _){
+            p1_[j] = p1_[j + mn] = p1[i];
+            p2_[j] = p2_[j + mn] = p2[i];
+            ++ i; ++ j;
+        }
+        j += m;
+    }
+    j = m;
+    while(i < mn){
+        for(int16_t _ = 0; _ < m; ++ _){
+            p1_[j] = p1[i];
+            p1_[j + mn] = - p1[i];
+            p2_[j] = p2[i];
+            p2_[j + mn] = - p2[i];
+            ++ i; ++ j; 
+        }
+        j += m;
+    }
+
+    for(i = mn; i >= n; i >>= 1){
+        for(j = 0; j < mn << 1; j += i){
+
+        }
+    }
+}
